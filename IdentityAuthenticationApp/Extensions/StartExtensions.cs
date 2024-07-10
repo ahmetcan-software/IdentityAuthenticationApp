2f3629a9647bc248daf43da82cfe33a04d8f82d8
ahmetcan-software <ahmetcansimsek3317@gmail.com>
2024-07-10 21:52:35 +0300

    IdentityAuthenticationApp
    AspNetCoreMvc-IdentityAuthenticationApp, ASP.NET Core Identity kullanarak kullanıcı kimlik doğrulama ve yetkilendirme sağlayan bir web uygulamasıdır

---
+++ IdentityAuthenticationApp/Extensions/StartExtensions.cs
@@ -0,0 +1,44 @@
+﻿using AspNetCoreMvc_IdentityAuthenticationApp.Data;
+using AspNetCoreMvc_IdentityAuthenticationApp.Identity.Models;
+
+namespace AspNetCoreMvc_IdentityAuthenticationApp.Extensions
+{
+    public static class StartExtensions
+    {
+        public static void AddIdentityExtensions(this IServiceCollection services)
+        {
+            services.AddIdentity<AppUser, AppRole>(
+                opt =>
+                {
+                    opt.Password.RequireNonAlphanumeric = false;
+                    opt.Password.RequiredLength = 3;
+                    opt.Password.RequireLowercase = false;
+                    opt.Password.RequireUppercase = false;
+                    opt.Password.RequireDigit = false;
+
+                    opt.User.RequireUniqueEmail = true;  //aynı email adresinin girilmesine izin vermez.
+
+                    //opt.User.AllowedUserNameCharacters = "abcdefghijklmnoprstuvyzqw0123456789";  //Kullanıcı adı girilirken sadece bu karakterlere izin verir.
+
+                    opt.Lockout.MaxFailedAccessAttempts = 3; //default=5
+                    opt.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(1); //default=5
+                }
+            ).AddEntityFrameworkStores<AppDbContext>();
+
+            services.ConfigureApplicationCookie(opt =>
+            {
+                opt.LoginPath = new PathString("/Account/Login");
+                opt.LogoutPath = new PathString("/Account/Logout");
+                //opt.AccessDeniedPath = new PathString("/Account/AccessDenied");
+                opt.ExpireTimeSpan = TimeSpan.FromMinutes(10);
+                opt.SlidingExpiration = true; //10 dk. dolmadan kullanıcı yeniden login olursa süre baştan başlar.
+
+                opt.Cookie = new CookieBuilder()
+                {
+                    Name = "Identity.App.Cookie",
+                    HttpOnly = true
+                };
+            });
+        }
+    }
+}
